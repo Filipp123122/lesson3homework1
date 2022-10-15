10a9cbe15d3957d89982d953a675eb9f352a86f8
unknown <filips333@mail.ru>
2022-10-15 23:07:43 +0300

    homework

---
+++ task25/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+
+int Exponentiation(int NumberA, int NumberB)
+{
+    int result = NumberA;
+    for(int i = 1; i < NumberB; i++)
+    {
+        result = result * NumberA;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число A");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+int exponentiation = Exponentiation(NumberA,NumberB);
+Console.WriteLine(exponentiation);
+
+
+
+
+/*
+int getResult (string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result <= 1)
+    {
+        Console.Write($"Ошибка ввода! Введите целое число больше единицы. {userInformation} ");
+    }
+    return result;
+}
+
+int userNumber1 = getResult("Number 1 = ");
+int userNumber2 = getResult("Number 2 = ");
+
+double findResult (int Number1, int Number2)
+{
+    return Math.Pow(Number1, Number2);
+}
+Console.WriteLine(findResult(userNumber1, userNumber2));
+*/
---
+++ task27/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int SummOfDigits(int Number)
+{         
+    if(Number < 0)
+    {
+        Number = Math.Abs(Number);
+    }
+    int Summ = 0;
+    while (Number > 0)
+    {
+        Summ = Summ + Number % 10;
+        Number = Number / 10;
+    }
+    return Summ;
+}
+int result = SummOfDigits(Number);
+Console.WriteLine(result);
+
+
